37bde7d030db8f39db6695c91f83f4cdcf5bbfa1
David Navarrete <david.navarrete@2brains.cl>
2022-07-20 08:46:41 -0400

    se mejora clase DataAPI

--- src/modules/Data.ts
+++ src/modules/Data.ts
@@ -1,6 +1,33 @@
-export default class DataAPI {
-    getDolar = async () => {
+export interface ResponseApi {
+    codigo: string,
+    serie: [ResponseApiSerie]
+}
+export interface ResponseApiSingle {
+    codigo: string,
+    serie: ResponseApiSerie
+}
+
+export interface ResponseApiSerie {
+    fecha: Date,
+    valor: number
+}
+
+export class DataAPI {
+    dolar: ResponseApi | ResponseApiSingle
+    constructor(dolarActual:ResponseApi | ResponseApiSingle ) {
+        this.dolar = dolarActual;
+    }
+    static getDolar = async (indice?:number) => {
         const a = await fetch('https://mindicador.cl/api/dolar');
-        return await a.json();
+        const json = await a.json();
+        let newDolar:ResponseApiSingle = {} as ResponseApiSingle;
+        const dolar:ResponseApi = json;
+        if (typeof indice !== 'undefined') {
+            newDolar.codigo = dolar.codigo;
+            newDolar.serie = {} as ResponseApiSerie
+            newDolar.serie = dolar.serie.filter((x,i) => i == indice )[0];
+            return new DataAPI(newDolar)
+        }
+        return new DataAPI(dolar);
     }
 }
